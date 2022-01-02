a6f7c55999f85ffee6069e833433c622b03fa202
komus-Israel <komolehinIsrael@gmail.com>
2022-01-02 12:40:09 +0100

    used post and get request

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api/', require('./users'))
 
-app.get('/', (req, res)=>{
-    res.send('Hello world')
-})
+
 
 app.listen(8000, ()=> {
     console.log('server is listening to port 8000')
--- users.js
+++ users.js
@@ -6,6 +6,36 @@ let users = require("./userDB")
 
 
 
-router.get('/', (req, res)=>{
-    res.send('Im here')
+router.get('/users', (req, res)=>{
+    res.json(users)
 })
+
+router.get('/user/:id', (req, res)=>{
+
+    const found = users.filter(user => user.id === parseInt(req.params.id))
+    res.send(found)
+
+})
+
+router.post('/user/new', (req, res)=>{
+
+    const newUser= {
+        id : uuid.v4(),
+        name : req.body.name,
+        email : req.body.email
+    }
+
+    users.push(newUser)
+
+    res.json("success")
+   
+})
+
+router.get('/update/:id', (req, res)=>{
+    const user = users.filter(user => user.id === parseInt(req.params.id))
+    user[0].id = 10
+
+    res.json("success")
+})
+
+module.exports = router
